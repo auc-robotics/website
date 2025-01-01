74e677f57a313e3e82285a5e72ee741d767d81f0
Farouk Tamer <farouktamer@aucegypt.edu>
2025-01-01 17:14:03 +0200

    create canvas line object

--- apps/web/src/components/canvas.tsx
+++ apps/web/src/components/canvas.tsx
@@ -2,49 +2,119 @@
 
 import { useEffect, useRef } from "react";
 
+type Point = [number, number];
+
+interface CanvasObject {
+  draw(canvas: CanvasCtx): void;
+}
+
+class Line implements CanvasObject {
+  points: Point[] = [];
+  lineWidth: number;
+  strokeStyle: string;
+
+  constructor(options: { lineWidth?: number; strokeStyle?: string } = {}) {
+    options.lineWidth ||= 1;
+    options.strokeStyle ||= "black";
+
+    this.lineWidth = options.lineWidth;
+    if (options.strokeStyle.startsWith("--")) {
+      this.strokeStyle = getComputedStyle(document.body).getPropertyValue(
+        options.strokeStyle,
+      );
+    } else {
+      this.strokeStyle = options.strokeStyle;
+    }
+  }
+
+  addPoint(x: number, y: number) {
+    this.points.push([x, y]);
+  }
+
+  draw(canvas: CanvasCtx) {
+    const c = canvas.ctx;
+    c.lineWidth = this.lineWidth;
+    c.strokeStyle = this.strokeStyle;
+    c.lineCap = "round";
+    c.lineJoin = "round";
+    c.beginPath();
+    const cPoints = this.points.map(([x, y]) =>
+      canvas.toPixelPerfectCoords(x, y),
+    );
+    c.moveTo(...cPoints[0]);
+    cPoints.slice(1).forEach(([x, y]) => {
+      c.lineTo(x, y);
+    });
+    c.stroke();
+  }
+}
+
 class CanvasCtx {
   ctx: CanvasRenderingContext2D;
   width: number;
   height: number;
+  preserveAspectRatio: boolean;
   afId: number = 0;
+  objects: CanvasObject[] = [];
 
   constructor(
     canvas: HTMLCanvasElement,
-    options: { width?: number; height?: number } = {},
+    options: {
+      width?: number;
+      height?: number;
+      preserveAspectRatio?: boolean;
+    } = {},
   ) {
     this.ctx = canvas.getContext("2d")!;
-    this.width = options.width || canvas.clientWidth;
-    this.height = options.height || canvas.clientHeight;
+    this.width = options.width || 1;
+    this.height = options.height || 1;
+    this.preserveAspectRatio = options.preserveAspectRatio || false;
     this.updateSize();
   }
 
-  updateSize() {
-    this.ctx.canvas.width = this.ctx.canvas.clientWidth;
-    this.ctx.canvas.height = this.ctx.canvas.clientHeight;
+  add(object: CanvasObject) {
+    this.objects.push(object);
   }
 
-  toCoords(x: number, y: number): [number, number] {
+  updateSize() {
+    const [cw, ch] = [
+      this.ctx.canvas.parentElement!.clientWidth,
+      this.ctx.canvas.parentElement!.clientHeight,
+    ].map(Math.floor);
+
+    this.ctx.canvas.width = cw;
+    this.ctx.canvas.height = ch;
+    if (this.preserveAspectRatio) {
+      const unit = Math.min(cw / this.width, ch / this.height);
+      this.width = cw / unit;
+      this.height = ch / unit;
+    }
+  }
+
+  toCoords(x: number, y: number): Point {
     return [
       (x / this.width) * this.ctx.canvas.width,
       (y / this.height) * this.ctx.canvas.height,
     ];
   }
-
-  toPixelPerfectCoords(x: number, y: number): [number, number] {
+  toPixelPerfectCoords(x: number, y: number): Point {
     const c = this.ctx;
     const [cx, cy] = this.toCoords(x, y);
-    const ppf = (c.lineWidth % 2) / 2;
+    const ppf = ((c.lineWidth + 1) % 2) / 2;
     return [Math.floor(cx) + ppf, Math.floor(cy) + ppf];
   }
 
   animate() {
     const c = this.ctx;
     c.clearRect(0, 0, c.canvas.width, c.canvas.height);
+    this.objects.forEach((object) => {
+      object.draw(this);
+    });
     this.afId = requestAnimationFrame(this.animate.bind(this));
   }
 
   stopAnimate() {
-    window.cancelAnimationFrame(this.afId);
+    cancelAnimationFrame(this.afId);
     this.afId = 0;
   }
 }
@@ -53,8 +123,16 @@ export default function Canvas() {
   const canvasRef = useRef<HTMLCanvasElement>(null);
 
   useEffect(() => {
-    const ctx = new CanvasCtx(canvasRef.current!, { width: 100, height: 100 });
-    console.log(ctx);
+    const ctx = new CanvasCtx(canvasRef.current!, {
+      height: 5,
+      preserveAspectRatio: true,
+    });
+    const line = new Line({ lineWidth: 5, strokeStyle: "--color-slate-100" });
+    line.addPoint(0, 0);
+    line.addPoint(1, 1);
+    line.addPoint(2, 1);
+    line.addPoint(3, 2);
+    ctx.add(line);
     ctx.animate();
 
     return () => {
@@ -62,5 +140,9 @@ export default function Canvas() {
     };
   });
 
-  return <canvas ref={canvasRef} className="size-full"></canvas>;
+  return (
+    <div className="size-full">
+      <canvas ref={canvasRef}></canvas>
+    </div>
+  );
 }

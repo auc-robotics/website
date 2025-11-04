0b66422b3e69c0aa6cfd0c967f13a0ff9de1a1c9
Farouk Tamer <farouktamer@aucegypt.edu>
2025-11-04 16:10:27 +0200

    add debounced resize observer to resize pcb background

--- apps/web/src/components/PCBBackground.tsx
+++ apps/web/src/components/PCBBackground.tsx
@@ -19,50 +19,72 @@ const translateGradients = (line: Line, dt: number) => {
 export default function PCBBackground() {
   const divRef = useRef<HTMLDivElement>(null);
   const ctxRef = useRef<CanvasCtx | null>(null);
+  const firstResize = useRef<bool>(true);
 
   const [width, setWidth] = useState(0);
   const [height, setHeight] = useState(0);
 
   useEffect(() => {
-    const cellSize = 40;
-    const div = divRef.current!;
+    let debounceTimer;
+    let interval;
+    const render = () => {
+      const cellSize = 32;
+      const div = divRef.current!;
 
-    const h = Math.ceil(div.clientHeight / cellSize);
-    const w = Math.ceil(div.clientWidth / cellSize);
-    setHeight(h);
-    setWidth(w);
+      const h = Math.ceil(div.clientHeight / cellSize);
+      const w = Math.ceil(div.clientWidth / cellSize);
+      setHeight(h);
+      setWidth(w);
 
-    const pcbDesign = generatePCBDesign(w + 2, h + 2);
-    const c = ctxRef.current!;
-    c.objects = [];
-    for (const line of pcbDesign) {
-      c.add(
-        new Line({
-          strokeStyle: "--color-slate-200",
-          lineWidth: 8,
-          points: line,
-          update: translateGradients,
-        }),
-      );
-    }
-
-    const interval = setInterval(() => {
+      const pcbDesign = generatePCBDesign(w + 2, h + 2);
       const c = ctxRef.current!;
-      const l = c.objects.length;
-      if (l == 0) return;
-      const line = c.objects[Math.floor(Math.random() * l)] as Line;
-      line.grads.push({
-        start: -2,
-        end: 0,
-        stops: [
-          [0, "--color-slate-200"],
-          [0.2, "--color-sky-300"],
-          [0.8, "--color-sky-300"],
-          [1, "--color-slate-200"],
-        ],
-      });
-    }, 8000 / pcbDesign.length);
-    return () => clearInterval(interval);
+      c.objects = [];
+      for (const line of pcbDesign) {
+        c.add(
+          new Line({
+            strokeStyle: "--color-slate-200",
+            lineWidth: 8,
+            points: line,
+            update: translateGradients,
+          }),
+        );
+      }
+
+      interval = setInterval(() => {
+        const c = ctxRef.current!;
+        const l = c.objects.length;
+        if (l == 0) return;
+        const line = c.objects[Math.floor(Math.random() * l)] as Line;
+        line.grads.push({
+          start: -2,
+          end: 0,
+          stops: [
+            [0, "--color-slate-200"],
+            [0.2, "--color-sky-300"],
+            [0.8, "--color-sky-300"],
+            [1, "--color-slate-200"],
+          ],
+        });
+      }, 8000 / pcbDesign.length);
+    };
+
+    render();
+    const observer = new ResizeObserver((entries) => {
+      clearTimeout(debounceTimer);
+      debounceTimer = setTimeout(() => {
+        if (firstResize.current) {
+          firstResize.current = false;
+          return;
+        }
+        clearInterval(interval);
+        render();
+      }, 300);
+    });
+    observer.observe(divRef.current);
+    return () => {
+      observer.disconnect();
+      clearTimeout(debounceTimer);
+    };
   }, []);
 
   return (

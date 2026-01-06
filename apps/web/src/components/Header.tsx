e83ab2242ac2e5102cfc517249d90b7039507dbe
pharaok <farouk.tamer03@gmail.com>
2026-01-06 20:24:27 +0200

    add summit link to header

--- apps/web/src/components/Header.tsx
+++ apps/web/src/components/Header.tsx
@@ -13,6 +13,9 @@ import SidebarModal from "@/components/Sidebar";
 const NavItems = () => (
   <>
     <li>
+      <Link href="https://www.summit.aucrobotics.org">Summit</Link>
+    </li>
+    <li>
       <Link href="/get-involved">Get Involved</Link>
     </li>
     <li>
